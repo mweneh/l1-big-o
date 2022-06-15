16d81d4938d692ac54f60babfe730acfaf6d3eb4
Ian Okumu <okumu.otsembo@gmail.com>
2022-06-15 13:25:40 +0300

    Morning: Finished with bug on log time

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const print = (value) => console.log(value)
 
 // user scores
-const scores = [99 ,80, 77, 86, 50, 33, 76, 90, 54, 66]
+const scores = [99 ,80, 77, 76, 86, 50, 33, 76, 90, 54, 66, 76]
 
 // TODO: IDENTIFY THE TIME COMPLEXITY OF THE FOLLOWING ARRAY METHODS
 // 1: PUSH
@@ -15,21 +15,52 @@ scores.pop()
 scores.unshift(88)
 
 // 4: forEach
-scores.forEach((element) => {print(element)})
+//scores.forEach((element) => {print(element)})
 
 
 // TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LINEAR TIME COMPLEXITY
 const findValueLinear = (value) => {
+    let index = []
+    for(let i = 0; i < scores.length; i++){
+        const score = scores[i]
+        print(`index: ${i}`)
+        if(score === value){
+            index.push(i)
+            //return index
+        }
+    }
+    return index
     // return the index of the value if present, return -1 if absent
 }
-// print(`190 is at index: ${findValueLinear(190)}`)
-// print(`76 is at index: ${findValueLinear(76)}`)
+//print(`190 is at index: ${findValueLinear(190)}`)
+//print(`76 is at index: ${findValueLinear(76)}`)
 
 
-// TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LOGARITHMIC TIME COMPLEXITY
-const findValueLog = (value) => {
+// TODO: CREATE A SIMPLE SEARCH ALGORITHM THAT USES LOGARITHMIC TIME COMPLEXITY (HAS BUG, WILL FIX)
+scores.sort()
+const findValueLog = (value, top, bottom) => {
+
+    let index = -1
+    const halfOps = (top + bottom) / 2
+    const half = parseInt(halfOps)
+
+    const valBeforeHalf = scores[half - 1]
+    if(value < valBeforeHalf){
+        // top half
+        findValueLog(value, half - 1, 0)
+    }else{
+        // bottom half
+        if(value === scores[half]) {
+            return half
+        }
+        findValueLog(value, scores.length - 1, half)
+    }
+
+    return index
+
     // return the index of the value if present, return -1 if absent
 }
+print(parseInt(3/2))
 
-// print(`90 is at index: ${findValueLog(90)}`)
-// print(`88 is at index: ${findValueLog(88)}`)
+ print(`90 is at index: ${findValueLog(90, scores.length -1 , 0 )}`)
+ print(`33 is at index: ${findValueLog(33, scores.length -1 , 0)}`)
